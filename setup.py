9afda96b2fb0595da8007c99e728dc3a332bde31
worepix <hry-kurasa@seznam.cz>
2019-05-03 13:57:42 +0200

    Add pattern

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+
+NAME = '{{NAME}}'
+DESCRIPTION = '{{DESCRIPTION}}'
+URL = '{{URL}}'
+EMAIL = '{{EMAIL}}'
+AUTHOR = '{{AUTHOR}}'
+REQUIRES_PYTHON = '{{REQUIRES_PYTHON}}'
+VERSION = '{{VERSION}}'
+LICENSE = '{{LICENSE}}'
+CLASSIFIERS = [
+        'Programming Language :: Python'
+    ]
+# CONSOLE_SCRIPTS = ['NAME=NAME:cli']
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    # entry_points={
+    #     'console_scripts': CONSOLE_SCRIPTS,
+    # }
+)
